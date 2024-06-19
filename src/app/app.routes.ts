a749b56e6908c1e9e14d0c505498d494558e7f73
Sw1ftFox <v.suvorkin2017@gmail.com>
2024-06-19 18:33:26 +0300

    files of two main components and one service were created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TileViewComponent } from './components/tile-view/tile-view.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tiles', component: TileViewComponent },
+  { path: 'list', component: ListViewComponent },
+  { path: '', redirectTo: '/tiles', pathMatch: 'full' }
+];
---
+++ src/app/components/tile-view/tile-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tile-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './tile-view.component.html',
+  styleUrl: './tile-view.component.scss'
+})
+export class TileViewComponent {
+
+}
